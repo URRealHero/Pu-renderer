ef18590ec6c2c767af51a402dc760184f6f93e32
PuShu <pushuabc@gmail.com>
2025-03-22 09:46:27 +0800

    fix typo and render a sphere

--- build/CMakeFiles/main.dir/src/main.cpp.o.d
+++ build/CMakeFiles/main.dir/src/main.cpp.o.d
@@ -156,4 +156,5 @@ CMakeFiles/main.dir/src/main.cpp.o: \
  /usr/include/c++/11/bits/locale_facets.tcc \
  /usr/include/c++/11/bits/basic_ios.tcc \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
- /usr/include/c++/11/bits/istream.tcc
+ /usr/include/c++/11/bits/istream.tcc \
+ /home/pu/PuFiles/3D/Pu-renderer/src/ray.h
